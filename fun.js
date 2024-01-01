3cfca37bb0dbee975b23dba7b828f721913e54b2
Ravikumar A <ravik@enqos.com>
2024-01-01 14:59:19 +0530

    inital commit

---
+++ fun.js
@@ -0,0 +1,34 @@
+//closures
+// function updateClickCount() {
+//     var counter = 0;
+//     ++counter;
+//     console.log("counter=>", counter)
+//     return counter
+//     // Do something with counter
+// }
+
+// function countWrapper() {
+//     var counter = 0;
+//     function updateClickCount() {
+//         ++counter;
+//         // Do something with counter
+//     }
+//     updateClickCount();
+//     console.log("counter=>", counter)
+//     return counter;
+// }
+
+
+var updateClickCount = (function(){
+    var counter = 0;
+
+    return function(){
+        ++counter;
+        document.getElementById("spnCount").innerHTML = counter;
+    }
+})();
+
+console.log(updateClickCount());
+
+
+//closures
